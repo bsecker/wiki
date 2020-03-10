ef7892dfebb238e23fb2f8747569ffb5d7e950e0
Benjamin Secker <benjamin.secker@gmail.com>
2020-03-10 15:30:23 +1300

    remove old scripts

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="WikiCLI",
+    version="0.1",
+    packages=find_packages(),
+    author="Benjamin Secker",
+    author_email="Benjamin.secker@gmail.com",
+    description="Markdown wiki CLI and scripts",
+    keywords="Markdown,wiki,scripts,cli,mdwiki",
+    url="github.com/bsecker/wiki",
+)
