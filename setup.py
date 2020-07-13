24c2f097d26ae9f6f760fbb2a24ba5bcc5e6dbc4
Benjamin Secker <benjamin.secker@gmail.com>
2020-07-13 23:04:06 +1200

    add setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,16 @@ setup(
     author_email="Benjamin.secker@gmail.com",
     description="Markdown wiki CLI and scripts",
     keywords="Markdown,wiki,scripts,cli,mdwiki",
-    url="github.com/bsecker/wiki",
+    url="https://github.com/bsecker/wiki",
+    scripts=[
+        'wiki/wiki.py',
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'PyInquirer',
+        'pyyaml',
+        'pdf2image==1.5.4',
+        'Pillow==6.0.0',
+        'requests'
+    ]
 )
